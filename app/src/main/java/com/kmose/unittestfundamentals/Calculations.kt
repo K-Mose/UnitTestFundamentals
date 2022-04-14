d1a245c016dfc0a9085c9cde394cf5c550cd59db
user <km_413@naver.com>
2022-04-14 23:26:39 +0900

    project setup

---
+++ app/src/main/java/com/kmose/unittestfundamentals/Calculations.kt
@@ -0,0 +1,6 @@
+package com.kmose.unittestfundamentals
+
+interface Calculations {
+    fun calculateCircumference(radius: Double): Double
+    fun calculateArea(radius: Double): Double
+}
---
+++ app/src/main/java/com/kmose/unittestfundamentals/MyCalc.kt
@@ -0,0 +1,9 @@
+package com.kmose.unittestfundamentals
+
+class MyCalc : Calculations{
+    private val pi = 3.14
+
+    override fun calculateCircumference(radius: Double): Double =  2 * pi * radius
+
+    override fun calculateArea(radius: Double): Double = pi * radius * radius
+}
